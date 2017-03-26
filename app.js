c173d496a8c11cb17978ce1517a0986fcd6117ea
steng <steven.teng@emc.com>
2017-03-25 22:38:48 -0400

    clean up a bit

--- app.js
+++ app.js
@@ -1,59 +1,14 @@
 var express = require('express')
 var app = express()
 var path = require('path')
-var cors = require('cors')
-
-
-/*app.use(cors);
-app.get('/product/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})*/
-
-app.use(function(req, res, next) {
- // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost/');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-// ## CORS middleware
-//
-// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
-/*var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
-*/
 
 app.use('/body_bg.jpg', express.static('body_bg.jpg'));
+
 app.get('/', function (req, res, next) {
   //res.send('Hello World!')
   res.sendFile(path.join(__dirname + '/index.html'));
-  console.log ("dir name " + __dirname);
 })
+
 
 app.get('/beta3', function (req, res, next) {
   //res.send('Hello World!')
